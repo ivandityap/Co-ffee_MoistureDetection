93de61cd823435d987c16d6ef2f49f160b11e01e
arsyakaukabi <arsyakaukabi@gmail.com>
2022-07-01 23:53:23 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 <p align="center">
-  <img width="500" alt="logo" src="https://github.com/ivandityap/Co-ffee_MoistureDetection/blob/main/Images/logo2.svg"/>
+  <img width="500" alt="logo" src="Images/logo2.svg"/>
 </p>
 
-[![](https://img.shields.io/badge/ID%20Team-C22_PC377-blue)](https://github.com/xrizer/Co-ffee)
-[![PyPI version](https://badge.fury.io/py/autokeras.svg)](https://badge.fury.io/py/autokeras)
+[![](https://img.shields.io/badge/ID%20Team-C22_PC377-blue)](https://github.com/arsyakaukabi/Co-ffee_BangkitCapstone)
 ![Python](https://img.shields.io/badge/python-v3.9.0+-success.svg)
 ![Tensorflow](https://img.shields.io/badge/tensorflow-v2.8.0+-success.svg)
-
 
 **Coffee Beans Moisture Detection with Fusioned Triple Deep Convolutional Neural Network**
 ## Graphical Abstract
@@ -16,9 +14,9 @@
 </p>
 
 ## Dataset
-**The dataset for this work is manually collected**
+**The dataset for this work is manually collected by [our team][1].**
 
-The dataset used for this work is images of coffee beans with different moisture. There is 5 different classification:
+The dataset used for this work is images of green coffee beans with different moisture. There is 5 different classification:
 
 ![dataset](Images/dataset.jpeg)
 
@@ -27,7 +25,7 @@ You can find the dataset here: **<a target=blank_ href="https://drive.google.com
 
 `Dataset Size : 411MB`
 
-
+[1]:(https://github.com/arsyakaukabi/Co-ffee_BangkitCapstone)
 ## Models
 Transfer learning and model fusion is used in this work. There are 3 fusioned model :
 - InceptionV3
@@ -50,6 +48,24 @@ Transfer learning and model fusion is used in this work. There are 3 fusioned mo
 7. After all T-DCNN models are built, you may now run the `testing.py` from the main `Co-ffee_MoistureDetection/` folder.
 8. Follow through the given instructions and make sure to use the test sample from the provided `/test/` folder
 ## Results
-###### Accuracy and loss graph after 25 epochs.
-![Co-ffee logo](https://github.com/ivandityap/Co-ffee_MoistureDetection/blob/main/Images/res1.png)
-![Co-ffee logo](https://github.com/ivandityap/Co-ffee_MoistureDetection/blob/main/Images/Screenshot%202022-06-12%20135755.png)
+<p align="center">
+    <img src="History/TDCNN.svg" alt="drawing" width="300"/>
+    
+<p align="center">
+    <b>Fig 2</b> Accuracy and loss graph after 25 epochs.
+</p>
+
+
+**Cloud Computing**
+>**[Disease classification API](https://github.com/xrizer/Co-ffee-Desease-API)**
+
+>**[Coffee beans Moisture level detection API](https://github.com/xrizer/Co-ffee-Desease-API
+)**
+
+**Mobile Development**
+
+>**[Project Android Studio](https://github.com/Rizalfirman165/co-ffee)**
+
+## Acknowledgment ##
+Thanks to [Bangkit Academy](https://grow.google/intl/id_id/bangkit/). Without its support, this work would not have become possible.
+
--- testing.py
+++ testing.py
@@ -1,39 +1,34 @@
+print('Loading ...')
+
 import os
 import logging
-
 import tensorflow as tf
-# import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import load_img, img_to_array
 
-from efficientnet.keras import preprocess_input
-
-import keras
-from keras.models import load_model
-from keras.utils import load_img, img_to_array
-# from keras import applications
-
+print('Packages loaded')
 tf.get_logger().setLevel(logging.ERROR)
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
+
 def load_models():
-    model = load_model("weights/TDCNN_1.h5")
+    model = load_model("Model\TDCNN.h5")
     print('model loading complete')
     return model
 
-def labels(n_stage):
-    label= {
-        '<13' : 0,
-        '<13.2' : 1,
-        '<14' : 2,
-        '<15' : 3,
-        '15>' : 4
-    }
-    return label
-
-def get_label(index,n_stage):
-   for class_string, class_index in labels(n_stage).items():
+labels = {
+    '13 %': 0,
+    '13.2 %': 1,
+    '14 %': 2,
+    '15 %': 3,
+    '> 15 %': 4
+}
+def get_label(index,labels=labels):
+   for class_string, class_index in labels.items():
       if class_index == index:
          return class_string
 
@@ -42,34 +37,30 @@ def show_predict(model, preprocessed_image):
     predicted_index = np.argmax(classes[0])
     confidence = max(100 * classes[0])
     return predicted_index, confidence
-    return show+predict
 
-def report(result, peluang, count):
-    for i in range(count):
-        print()
-        print(f'Result : {get_label(result,i)}')
-        print(f'Confidence level : {peluang:.2f} %')
+def report(result, peluang):
+    hasil = f'{get_label(result)} ({peluang:.2f} %)'
+    return hasil
     
 def main(img_path, model):
-    img=load_img(img_path,target_size=(224,224))
+    img=load_img(img_path, target_size=(224, 224))
     x = img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     
-    index, confidence = 0, 0
-    index, confidence = show_predict(model,x)
-    count = 1
-    return index, confidence, count
+    result, peluang = show_predict(model, x)
+
+    return result, peluang
 
 if __name__ == "__main__":
-    path = 'test1/'
+    path = 'Test/'
     model = load_models()
 
     while True:
         while True:
             try:
-                img_path = path + input('Please enter a file from your test folder with the coffee leaf images ex: (2.jpg):')
-                result, peluang, count = main(img_path, model)
+                img_path = path + input('input image :')
+                result, peluang = main(img_path, model)
             except IOError:
                 print(
                     "The image was not found or is invalid. Please try again, or press CTRL+C to exit the program..."
@@ -78,14 +69,12 @@ if __name__ == "__main__":
                 break
         
         print(img_path)
-        report(result, peluang, count)
+        hasil = report(result, peluang)
+        print(hasil)
         print("\nClose the image window to continue. . .")
-        img=load_img(img_path,target_size=(224,224))
+        img = load_img(img_path, target_size=(224, 224))
         plt.imshow(img)
         plt.show()
-
-
-
 
 
 
